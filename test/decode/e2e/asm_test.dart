de888a73f29b142f898b8e72c99618af554f2ca8
Matan Lurey <matan@lurey.org>
2020-06-30 13:32:17 -0700

    Added some e2e golden tests.

--- test/decode/e2e/asm_test.dart
+++ test/decode/e2e/asm_test.dart
@@ -6,13 +6,14 @@ import 'package:armv4t/src/decode/arm/format.dart';
 import 'package:armv4t/src/decode/arm/instruction.dart';
 import 'package:armv4t/src/decode/arm/printer.dart';
 import 'package:path/path.dart' as path;
+import 'package:goldenrod/goldenrod.dart';
 import 'package:test/test.dart';
 
 // For every `.bin` file in this directory, we decode (dissasemble) the file
 // into a .d.asm file. This test verifies that the ouput .d.asm files match what
 // would be output (a sort of golden-file).
 void main() async {
-  final asmDir = Directory(path.join('test', 'decode', 'asm'));
+  final asmDir = Directory(path.join('test', 'decode', 'e2e', 'asm'));
   if (!await asmDir.exists()) {
     fail('Could not find ${asmDir.path}.');
   }
@@ -35,17 +36,19 @@ Future<void> _testBin(File file) async {
   test('${file.path}', () async {
     final bytes = await file.readAsBytes();
     final encoded = Uint32List.view(bytes.buffer);
-    final output = File(path.withoutExtension(file.path) + '.d.asm');
     final printer = ArmInstructionPrinter();
-    await output.writeAsString(encoded.map((e) {
-      String s;
-      try {
-        final i = decode(e);
-        s = i.accept(printer);
-      } catch (e) {
-        s = '???? <${e.runtimeType}>';
-      }
-      return '$s'.padRight(32) + '; 0x${e.toRadixString(16).toUpperCase()}';
-    }).join('\n'));
+    final output = '; Automatically generated\n\n' +
+        encoded.map((e) {
+          String s;
+          try {
+            final i = decode(e);
+            s = i.accept(printer);
+          } catch (e) {
+            s = '???? <${e.runtimeType}>';
+          }
+          return '$s'.padRight(32) + '; 0x${e.toRadixString(16)}';
+        }).join('\n');
+    final outputPath = path.withoutExtension(file.path) + '.out';
+    expect(output, await matchesGoldenText(file: outputPath));
   });
 }
