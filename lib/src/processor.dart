4b41a6c4d092fb2d1901cc24a12dc71444fbbaeb
Matan Lurey <matan@lurey.org>
2020-07-07 21:10:29 -0700

    Start on Arm7Processor.

---
+++ lib/src/processor.dart
@@ -0,0 +1,137 @@
+import 'dart:typed_data';
+
+import 'package:armv4t/src/common/assert.dart';
+import 'package:binary/binary.dart';
+import 'package:meta/meta.dart';
+
+/// Represents an operating mode of the ARM processor.
+@immutable
+@sealed
+class ArmOperatingMode {
+  static const usr = ArmOperatingMode._(0x10, 'usr');
+  static const fiq = ArmOperatingMode._(0x11, 'fiq');
+  static const irq = ArmOperatingMode._(0x12, 'irq');
+  static const svc = ArmOperatingMode._(0x13, 'svc');
+  static const abt = ArmOperatingMode._(0x17, 'abt');
+  static const und = ArmOperatingMode._(0x1b, 'und');
+  static const sys = ArmOperatingMode._(0x1f, 'sys');
+
+  /// 4-bit value that represents this mode.
+  final int value;
+
+  /// Name of the operating mode.
+  final String name;
+
+  const ArmOperatingMode._(this.value, this.name);
+
+  @override
+  String toString() {
+    if (assertionsEnabled) {
+      return 'ArmOperatingMode { 0x${value.toRadixString(16)}: $name }';
+    } else {
+      return super.toString();
+    }
+  }
+}
+
+/// An emulated ARMv7 CPU.
+@sealed
+abstract class Arm7Processor {
+  /// Creates a new [Arm7Processor], which uses the `ARMv4T` instruction set.
+  ///
+  /// May optionally provide an initial set of [registers], but these are
+  /// expected to contain valid elements and undefined behavior may occur if
+  /// they were not originally created by a compatible instance of this class.
+  factory Arm7Processor({Uint32List registers}) = _Arm7Processor;
+
+  /// Reads the current value stored in the register [index].
+  Uint32 operator [](int index);
+
+  /// Writes [value] into the register [index].
+  void operator []=(int index, Uint32 value);
+
+  /// Returns a copy of the current state of the registers.
+  Uint32List copyRegisters();
+}
+
+class _Arm7Processor implements Arm7Processor {
+  /// Number of registers that are available normally.
+  ///
+  /// This includes ro->r14, as well as r15 (the program counter).
+  static const _userRegisters = 16;
+
+  static const _fiqRegsiters = 7;
+  static const _svcRegisters = 2;
+  static const _abtRegisters = 2;
+  static const _irqRegisters = 2;
+  static const _undRegistesr = 2;
+
+  /// Table describing additional registers available per [ArmOperatingMode].
+  static const _bankedRegisters = 0 +
+      /**/
+      _fiqRegsiters +
+      _svcRegisters +
+      _abtRegisters +
+      _irqRegisters +
+      _undRegistesr;
+
+  /// Number of status registers.
+  ///
+  /// This includes the CPSR ([ArmOperatingMode.usr] and [ArmOperatingMode.sys])
+  /// as well the various SPSRs for the remaining operating modes, which are
+  /// banked for usage only in that operating mode.
+  static const _statusRegisters = 6;
+
+  /// Number of 32-bit registers needed to represent the physical registers.
+  static const _physicalRegisters = 0 +
+      /**/
+      _userRegisters +
+      _bankedRegisters +
+      _statusRegisters;
+
+  /// A combination of both current, banked, and status registers.
+  ///
+  /// - `00 -> 15`: General registers.
+  /// - `16`:       `CPSR`.
+  /// - `17 -> 23`: Banked regsiters (`FIQ`).
+  /// - `24`:       `SPSR_FIQ`.
+  /// - `25 -> 26`: Banked registers (`SVC`).
+  /// - `27`:       `SPSR_SVC`.
+  /// - `28 -> 29`: Banked registers (`ABT`).
+  /// - `30`:       `SPSR_ABT`
+  /// - `31 -> 32`: Banked registers (`IRQ`).
+  /// - `33`:       `SPSR_IRQ`.
+  /// - `34 -> 35`: Banked registers (`UND`).
+  /// - `36`:       `SPSR_UND`.
+  final Uint32List _registers;
+
+  factory _Arm7Processor({Uint32List registers}) {
+    if (registers == null) {
+      registers = Uint32List(_physicalRegisters);
+    } else if (registers.length != _physicalRegisters) {
+      throw ArgumentError.value(
+        registers,
+        'registers',
+        'Requires exact length of $_physicalRegisters, got ${registers.length}',
+      );
+    }
+    return _Arm7Processor._(registers);
+  }
+
+  const _Arm7Processor._(this._registers);
+
+  @override
+  Uint32 operator [](int index) {
+    RangeError.checkValueInInterval(index, 0, _userRegisters);
+    return _registers.getBoxed(index);
+  }
+
+  @override
+  void operator []=(int index, Uint32 value) {
+    RangeError.checkValueInInterval(index, 0, _userRegisters);
+    _registers[index] = value.value;
+  }
+
+  @override
+  Uint32List copyRegisters() => Uint32List.fromList(_registers);
+}
